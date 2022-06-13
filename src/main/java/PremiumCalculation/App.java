9a45e1e10ccc8f3b1e94b3ed88280b353be60388
A1tred <socold800@gmail.com>
2022-06-13 15:45:00 +0300

    feat wtf

---
+++ src/main/java/PremiumCalculation/App.java
@@ -0,0 +1,11 @@
+package PremiumCalculation;
+
+class App {
+
+    public static void main(String[] args) {
+        Program program = new Program(
+                new UserChoice()
+        );
+        program.startProgram();
+    }
+}
